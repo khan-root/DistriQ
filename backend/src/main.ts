bcbe8eb72861a22cdb33a6d3e94b8ab80b4af270
Sarmad Faizan <sarmadf49@gmail.com>
2025-07-21 23:49:02 +0500

    implement api prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe());
+  const prefix = configService.get<string>('API_PREFIX') || 'api';
+  app.setGlobalPrefix(prefix);
   const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
 }
