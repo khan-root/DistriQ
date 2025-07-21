875efc73917afdf0a088ebbad00076f360434ec2
Sarmad Faizan <sarmadf49@gmail.com>
2025-07-22 00:05:13 +0500

    create login and register route also create respective dtos

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterDto } from './dtos/register.dto';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dtos/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+  }
+
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { RegisterDto } from './dtos/register.dto';
+import { LoginDto } from './dtos/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async register(registerDto: RegisterDto) {
+    try {
+      console.log(registerDto);
+    } catch (error) {
+      throw new BadRequestException(error?.message);
+    }
+  }
+
+  async login(loginDto: LoginDto) {
+    try {
+      console.log(loginDto);
+    } catch (error) {
+      throw new BadRequestException(error?.message);
+    }
+  }
+}
